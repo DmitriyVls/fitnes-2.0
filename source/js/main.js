a40b44b43cf36b760e669bb0136833c2738d495a
dmitriyvls <hlooba@mail.ru>
2023-07-24 00:14:09 +0300

    swiper

--- source/js/main.js
+++ source/js/main.js
@@ -6,3 +6,18 @@ new Swiper('.swiper', {
     prevEl: '.slider__button--left',
   },
 });
+// eslint-disable-next-line no-new
+new Swiper('.trainers__slider', {
+  navigation: {
+    nextEl: '.slider__button--right',
+    prevEl: '.slider__button--left',
+  },
+  breakpoints: {
+    1200: {
+      slidesPerView: 4,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+  },
+});
